32463bb84c37bce2fa8fdfe295197bb7bcfe3211
김서연 <86418674+flowersayo@users.noreply.github.com>
2023-06-18 16:22:52 +0900

    feat: demo main page layout

--- src/pages/Demo/DemoMainPage.tsx
+++ src/pages/Demo/DemoMainPage.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import MainBtn from '../../components/MainBtn';
+import { Headline } from '../../styles/Typography';
+import { Box } from '@mui/material';
+import SizedBox from '../../components/SizedBox';
+import { useNavigate } from 'react-router-dom';
 const DemoMainPage = () => {
-  return <div>데모데이 투표 메인 페이지</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '50px',
+      }}
+    >
+      <Headline>데모데이 투표 </Headline>
+      <SizedBox height={'20px'} />
+      <MainBtn onClick={() => navigate('/demo/vote')}>투표하기</MainBtn>
+      <MainBtn onClick={() => navigate('/demo/result')}>결과보기</MainBtn>
+    </Box>
+  );
 };
 
 export default DemoMainPage;
