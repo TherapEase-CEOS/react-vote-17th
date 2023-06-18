fa1dd4dd4b5b3f2fdc587f62fed594e83ba59ac6
권가은 <aileen1007@ewhain.net>
2023-06-18 18:29:27 +0900

    feat: part vote page ui

---
+++ src/constants/dummyData.ts
@@ -0,0 +1,12 @@
+export const DUMMY_MEMBERS = [
+  { teamName: 'TherapEase', userName: '권가은' },
+  { teamName: 'TherapEase', userName: '김서연' },
+  { teamName: 'bariBari', userName: '오예린' },
+  { teamName: 'bariBari', userName: '최민주' },
+  { teamName: 'Hooking', userName: '장효신' },
+  { teamName: 'Hooking', userName: '김문기' },
+  { teamName: 'DanSupport', userName: '노수진' },
+  { teamName: 'DanSupport', userName: '신유진' },
+  { teamName: 'Repick', userName: '이예지' },
+  { teamName: 'Repick', userName: '배성준' },
+];
---
+++ src/pages/Part/PartVoteCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { Button } from '@mui/material';
+import { CustomFont } from '../../styles/Typography';
+
+interface PartVoteCardProps {
+  memberInfo: { teamName: string; userName: string };
+  isSelected: boolean;
+  setSelectedMember: (value: { teamName: string; userName: string }) => void;
+}
+
+const PartVoteCard = ({
+  memberInfo,
+  isSelected,
+  setSelectedMember,
+}: PartVoteCardProps) => {
+  const { teamName, userName } = memberInfo;
+
+  const handleCardClick = () => {
+    if (isSelected) {
+      setSelectedMember({ teamName: '', userName: '' });
+    } else {
+      setSelectedMember(memberInfo);
+    }
+  };
+
+  return (
+    <Button
+      variant="outlined"
+      sx={{
+        width: '180px',
+        height: '100px',
+        display: 'flex',
+        flexDirection: 'column',
+        border: '2px solid',
+        borderRadius: '15px',
+        borderColor: 'primary.main',
+        color: `${isSelected ? 'white' : 'black'}`,
+        backgroundColor: `${isSelected ? 'primary.main' : 'white'}`,
+        '&:hover': {
+          color: 'white',
+        },
+      }}
+      onClick={handleCardClick}
+    >
+      <CustomFont inherit size="0.8rem">{`${teamName}\n`}</CustomFont>
+      <CustomFont
+        inherit
+        weight={700}
+        size="1.3rem"
+      >{`${userName}`}</CustomFont>
+    </Button>
+  );
+};
+
+export default PartVoteCard;
