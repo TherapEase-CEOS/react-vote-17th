2869bf1a90383f1bb622f4499169b4a9b526259f
권가은 <aileen1007@ewhain.net>
2023-06-18 11:18:27 +0900

    feat: theme.css 적용

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#384084',
+    },
+  },
+  typography: {
+    fontFamily: 'Inter, sans-serif',
+  },
+});
+
+export default theme;
